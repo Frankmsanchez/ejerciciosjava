29d565c3b0caf1fd6a1d972795d3480b7b60aa9f
Frank <frankmorasanchez@gmail.com>
2023-01-23 18:26:16 -0600

    Ejersicio tema1 java

---
+++ src/EjersicioTema1.java
@@ -0,0 +1,39 @@
+public class EjersicioTema1 {
+    public static void main(String[] args) {
+        System.out.println("Estos son los tipos de datos vistos");
+
+        byte var1 = 10;
+        System.out.println(var1);
+
+        short var2= 5;
+        System.out.println(var2);
+
+        int var3 = 100;
+        System.out.println(var3);
+
+        Long var4 = 10000L;
+        System.out.println(var4);
+        // decimals
+        float variable= 20.2f;
+        System.out.println(variable);
+
+        double varnum= 20.5d;
+        System.out.println(varnum);
+
+        //2. booleans
+        boolean cierto = true;
+        System.out.println(cierto);
+
+        boolean falso = false;
+        System.out.println(falso);
+
+
+
+        //2. text
+        char varchar= 'a';
+        System.out.println(varchar);
+
+        String cadenaCarateres= "Hola mundo";
+        System.out.println(cadenaCarateres);
+    }
+}
